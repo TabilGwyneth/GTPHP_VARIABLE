e939b3ae4e1159086ca83f739437e5c50e2c51b9
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-10-13 09:37:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 $first_variable = "I did not him!";
 $second_variable = "oh! Hi Mark!";
 
-echo "1st He says: ";
+echo "1st He says: '";
 echo $first_variable;
 echo "<br></br>";
-echo "and then he said: ";
+echo "and then he said: '";
 echo $second_variable;
 echo "<br></br>";
 
