049060298808510c8dc6111804f9d181d25f6ca9
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-10-13 09:26:49 +0100

    uploaded a php file with variables

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$first_variable = "I did not him!";
+$second_variable = "oh! Hi Mark!";
+
+echo "1st He says: '";
+echo $first_variable;
+echo "'<br></br>";
+echo "and then he said: '";
+echo $second_variable;
+echo "'<br></br>";
+
+?>
